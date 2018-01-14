be5e8013af7e99e8313fdab018d81d06eae83c67
ClarenceNeo <niuzhiyuan94@gmail.com>
2018-01-14 16:09:57 +0800

    add register validate

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -13,7 +13,15 @@
  import router from './routes'
  import App from './components/App'
 
+ import zh_CN from './locale/zh_CN';
+ import VeeValidate, { Validator } from 'vee-validate';
+
+ Validator.localize('zh_CN', zh_CN);
+
  Vue.use(VueRouter)
+ Vue.use(VeeValidate, {
+   locale: 'zh_CN'
+ })
  Vue.component('app', App)
 
  new Vue({
